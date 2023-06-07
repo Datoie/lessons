6741b9909c59a5fe1228e396232fbbcef886d362
Davit Grdzelishvili <60033691+Datoie@users.noreply.github.com>
2023-06-07 17:11:01 +0400

    add day 1 to github <3

---
+++ day_1/lsesson_1.py
@@ -0,0 +1,7 @@
+name = "Davit"
+surname = "Grdzelishvili"
+knowsLittlePrograming = True
+age = 19
+
+
+print("hello my name is" + " "+ name + " " + surname + " "+ "I am" + " " + str(age) + " " + "years old and o knew little coding" + " " + str(knowsLittlePrograming))
