b0923e27f4b2f9a73fda781dc8577e120fa8bc0a
Davit Grdzelishvili <60033691+Datoie@users.noreply.github.com>
2023-06-08 00:12:53 +0400

    I fiinaly made this script i thinck 1.30 hours

---
+++ day_3/day-3.py
@@ -0,0 +1,86 @@
+# my_name = "davit"
+
+# if 10>5:
+#     print("u are true")
+
+# if True:
+#     print("u are rigth")
+
+# if "v" in my_name:
+#     print("v are in the object")
+
+# my_name = "davit"
+# my_surname = "grdzelishvili"
+# my_age = 19
+
+# my_sentence = "aa {} bb {} cc {}".format(my_name, my_surname, my_age)
+
+# print(my_sentence)
+
+
+# print("hello my name is {0} {2} I am {1} years old".format("davit", 28, "grdzelishvili"))
+# #format method can change values inside of {}
+
+# my_name = "davit"
+# if "1" in my_name:
+#     print("u are true")
+# else:
+#     print("u are wrong")
+
+
+#input
+#output
+
+
+# my_name = input("enter ure name: ")
+# what i can write in oject wich ecuals input it saves on our object and where i want i can use it
+# print("my name is " + my_name)
+
+
+# my_age = 19
+# my_age += 3
+# print(my_age) 
+
+# my_name = input("what is ure name: ")
+# my_surname = input("what is ure surname: ")
+# my_age = int(input("what is ure age: "))
+# new_age = int(my_age) + 3
+
+# print("hello dear {} {} is ure age {} years old? veey nice".format(my_name, my_surname, my_age))
+# print("after 3 years u will become {}".format(new_age))
+
+
+# value1 = int(input("number one: "))
+# value2 = int(input("number two: ")) 
+# result = value2 * value1
+
+# if result < 100:
+#     print("u winn")
+# else:
+#     print(result)
+
+# print(20 % 6) # ნაშთს გვეუბნება
+
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+num3 = int(input("enter num3: "))
+
+if num1 % 2 == 0:
+    num1 = num1 * 0
+else:
+    num1 = num1
+
+
+if num2 % 2 == 0:
+    num2 = num2 * 0
+else:
+    num2 = num2
+
+if num3 % 2 == 0:
+    num3 = num3 * 0
+else:
+    num3 = num3
+
+result = num3 + num2 + num1 
+
+print(result)
