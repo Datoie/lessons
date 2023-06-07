b8c8caaa2292d1f73de5a8f1bbd3917847319d78
Davit Grdzelishvili <60033691+Datoie@users.noreply.github.com>
2023-06-07 18:22:03 +0400

    finish day 2 to github

---
+++ day_2/day-2.py
@@ -0,0 +1,66 @@
+# name = "nika"
+
+# print(len(name)) #lne Length
+#            #4
+
+
+# my_sentence = """Hello, I am reminding that study is studing at 19 september
+# 19 please
+# dont skip lesson""" #this """ branches use for long sentencises
+ 
+# print(my_sentence)
+
+# knows_programing = True
+
+# if 10>5:
+#     print("U are true")
+#     #indentations must be compleate thats mean u nedd space between line and code
+
+# if True:
+#     print("U are absoultly true")
+
+# full_name = "davit grdzelishvili"
+#            012345678910111213...   index always start with 0
+# print(len(full_name))
+
+#imagine every string is list
+# print(full_name[10])
+#position = index are sinosnims
+
+# print("1" in full_name)
+
+# if we want to get someting letter from "full_name" we are typing to our code
+# in or not in
+
+full_name = "davit grdzelishvili"
+# print(full_name[0:3])
+# #we can slice our code with there
+# print(full_name[0:7:2])
+# #start:finish:jump = step
+
+# print(full_name[:5])
+# print(full_name[5:])
+
+
+# #we can print last word in our code
+
+# print(full_name[-4]+"abc" + " " + "tupucu" + " " + "tapc")
+# print(full_name[-1:-7:-1])
+
+
+#sting has methods
+# 1) sting.upperr()
+# 2) len(string)
+
+# print(full_name.upper())
+
+name2 = "     DAVIT        "
+
+# print(name2.lower())
+
+# print(name2.strip())
+
+print(name2.replace("A", "@"))
+
+name3 = "saba vashakmadze"
+print(name3.replace("a", "$$"))
